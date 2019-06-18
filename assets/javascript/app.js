0f1021f83d4d2e10697b182c7a6e29c569beddcf
Red Tail <redtail814@gmail.com>
2019-06-17 22:48:23 -0400

    Finished function to compare user's choices and answers. Added scoreboard.  Added styling to new divs.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,34 +1,33 @@
 window.onload = function () {
-    $("#start-button").on("click", game.triviaTimer);
+    $("#start-button").click(game.triviaTimer);
     $("#finish-button").hide();
+    $("#timer").css('visibility', 'hidden');
 };
 
 var game = {
 
     timer: 0,
-    seconds: 10,
+    seconds: 45,
 
+    //  Starts and shows the trivia game timer.  Shows questions.
     triviaTimer: function () {
         $("#start-button").hide();
-        $("#timer").html('<div>00:' + game.seconds + '</div>').addClass('theTime');
+        $("#timer").css('visibility', 'visible');
+        $("#timer").html('<div>00:' + game.seconds);
         game.timer = setInterval(game.count, 1000);
-        trivia.questionForm();
+        game.questionForm();
     },
 
+    //  THE FINAL COUNTDOWN!!
     count: function () {
         game.seconds--;
         game.checkTime();
-        $("#timer").html('<div>00:' + game.seconds + '</div>').addClass('theTime');
+        $("#timer").html('<div>00:' + game.seconds);
         if (game.seconds < 1) {
             game.stopGame();
         };
     },
-
-    stopGame: function () {
-        clearInterval(game.timer);
-        trivia.checkScore();
-    },
-
+    
     // Adds a zero in front of numbers < 10
     checkTime: function () {
         if (game.seconds < 10) {
@@ -36,50 +35,88 @@ var game = {
         };
         return game.seconds;
     },
-}
 
-//   Object containing trivia questions and answers.  Using jquery to add all ten questions to page.
-var trivia = {
+    //  Stops timer and checks answers.
+    stopGame: function () {
+        clearInterval(game.timer);
+        game.checkScore();
+    },
 
+    //  Trivia questions and answers.  Using jquery to add all ten questions to page.
     questionForm: function () {
 
         var questionForm = $("#question-form");
 
         for (let i = 0; i < questionArr.length; i++) {
 
-            questionForm.append('<div>' + questionArr[i].question + '<div>').addClass("mt-4");
+            //  Creates div for Question.
+            questionForm.append('<div class="question russo mt-4">' + questionArr[i].question);
 
+            //  Variables to hold multiple choice answers.
             let answerOne = questionArr[i].choices[0];
             let answerTwo = questionArr[i].choices[1];
             let answerThree = questionArr[i].choices[2];
             let answerFour = questionArr[i].choices[3];
 
-            questionForm.append('<div class="form-check my-2"><input class="form-check-input" type="radio" name="radio-group' + i + '" id="radio' + i + '"><label class="form-check-label" id="radio' + i + 'label" for="radio' + i + '">' + answerOne + '</label></div>');
+            //  Creates seperate divs to hold the multiple choice answers individually.
+            questionForm.append('<div class="answer russo form-check my-2"><input class="form-check-input" type="radio" name="radio-group' + i + '" id="radio' + i + '"><label class="form-check-label" id="radio' + i + 'label" for="radio' + i + '">' + answerOne);
 
-            questionForm.append('<div class="form-check my-2"><input class="form-check-input" type="radio" name="radio-group' + i + '" id="radio' + i + '"><label class="form-check-label" id="radio' + i + 'label" for="radio' + i + '">' + answerTwo + '</label></div>');
+            questionForm.append('<div class="answer russo form-check my-2"><input class="form-check-input" type="radio" name="radio-group' + i + '" id="radio' + i + '"><label class="form-check-label" id="radio' + i + 'label" for="radio' + i + '">' + answerTwo);
 
-            questionForm.append('<div class="form-check my-2"><input class="form-check-input" type="radio" name="radio-group' + i + '" id="radio' + i + '"><label class="form-check-label" id="radio' + i + 'label" for="radio' + i + '">' + answerThree + '</label></div>');
+            questionForm.append('<div class="answer russo form-check my-2"><input class="form-check-input" type="radio" name="radio-group' + i + '" id="radio' + i + '"><label class="form-check-label" id="radio' + i + 'label" for="radio' + i + '">' + answerThree);
 
-            questionForm.append('<div class="form-check my-2 mb-5"><input class="form-check-input" type="radio" name="radio-group' + i + '" id="radio' + i + '"><label class="form-check-label" id="radio' + i + 'label" for="radio' + i + '">' + answerFour + '</label></div>');
+            questionForm.append('<div class="answer russo form-check my-2"><input class="form-check-input" type="radio" name="radio-group' + i + '" id="radio' + i + '"><label class="form-check-label" id="radio' + i + 'label" for="radio' + i + '">' + answerFour);
         }
 
-        var finish = $("#finish-button");
-        finish.show();
-        $("#finish-button").on("click", game.stopGame);
+        //  Shows the "Submit" button to submit answers.
+        $("#finish-button").show();
+        $("#finish-button").click(game.stopGame);
     },
 
-    
-
-
-    
-
     //  Funtion to check amount of correct and incorrect answers.
-    // checkScore: function () {
+    checkScore: function () {
 
-    // }
+        var answer;
+        var userChoice;
+        
+        correctAnswers = 0;
+        wrongAnswers = 0;
 
+        // loop through to compare the text of the label with the user answers
+        // increment score counts appropriately
+        for (let i = 0; i < questionArr.length; i++) {
+            
+
+            answer = questionArr[i].answer;
+            userChoice = $('input[id=radio'+i+']:checked + label').text();
+
+            if (userChoice === answer) {
+                correctAnswers++;
+                console.log(correctAnswers);
+            }
+
+            else if (userChoice !== answer || userChoice === "") {
+                wrongAnswers++;
+            }
+        }
+
+        game.scoreboard();
+    },
+
+    //  Function to show scoreboard.
+    scoreboard: function () {
+
+        scoreboard = $("#scoreboard");
+
+        $("#timer").hide();
+        $("#question-form").hide();
+        $("#finish-button").hide();
+
+        scoreboard.append('<div class="orbitron my-5">Correct: ' + correctAnswers);
+        scoreboard.append('<div class="orbitron my-5">Wrong: ' + wrongAnswers);
+    
+    }
 }
-
 
 //  Array of questions, multiple choices, and correct answer.
 var questionArr = [
